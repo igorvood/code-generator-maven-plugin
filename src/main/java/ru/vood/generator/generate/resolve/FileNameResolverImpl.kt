c136c22970b79c024ce6c24e4c6f0307310fd00d
vood <balitsky_igor@list.ru>
2019-12-20 18:56:44 +0300

    RegexpTEst

--- src/main/java/ru/vood/generator/generate/resolve/FileNameResolverImpl.kt
+++ src/main/java/ru/vood/generator/generate/resolve/FileNameResolverImpl.kt
@@ -9,20 +9,19 @@ class FileNameResolverImpl : FileNameResolver {
     override fun resolveFileByContent(typeFile: TypeFile, text: String): FileProperty {
         val patternPack: Pattern = Pattern.compile(typeFile.packageRegexp, Pattern.MULTILINE)
         val matcherPack: Matcher = patternPack.matcher(text)
+        var err = ""
         var pack: String? = null
         if (matcherPack.find()) {
             pack = matcherPack.group(1)
-        }
+        } else err = "Can not resolve package name, regexp=${typeFile.packageRegexp}."
         val patternClass: Pattern = Pattern.compile(typeFile.classNameRegexp, Pattern.MULTILINE)
         val matcherClass: Matcher = patternClass.matcher(text)
         var clazz: String? = null
 
         if (matcherClass.find()) {
             clazz = matcherClass.group(6)
-        }
-        var err = ""
-        if (pack == null) err = "Can not resolve package name, regexp=${typeFile.packageRegexp}."
-        if (clazz == null) err += " Can not resolve class name, regexp=${typeFile.classNameRegexp}."
+        } else err += " Can not resolve class name, regexp=${typeFile.classNameRegexp}."
+
         if (err.isNotEmpty()) throw FileNameResolverException("$err. File text:\n$text")
         return FileProperty(fileName = clazz!!, packageStr = pack!!, type = typeFile)
     }
