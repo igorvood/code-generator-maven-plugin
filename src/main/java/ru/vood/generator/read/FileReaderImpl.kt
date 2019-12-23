2e5eb5b84892007e7fea8c9b919665354680220c
vood <balitsky_igor@list.ru>
2019-12-23 21:13:07 +0300

    Plugin param

--- src/main/java/ru/vood/generator/read/FileReaderImpl.kt
+++ src/main/java/ru/vood/generator/read/FileReaderImpl.kt
@@ -5,17 +5,14 @@ import java.util.*
 
 class FileReaderImpl : FileReader {
 
-    override fun readFile(path: String): String {
-        val sb = StringBuilder(getResourceUrl(path))
-        return sb.toString()
-    }
-
-    private fun getResourceUrl(filename: String): String {
+    override fun readFile(filename: String): String {
         val resource = javaClass.classLoader.getResource(filename)
         Objects.requireNonNull(resource, "file $filename is not found!")
         val file = File(resource.path)
-        if (!file.exists()) throw IllegalStateException("file ${file.absolutePath} not found")
-        return file.readText()
+        if (!file.exists()) {
+            throw IllegalStateException("file ${file.absolutePath} not found")
+        } else return file.readText()
+
     }
 
 }
